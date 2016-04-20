24e71c6a1c0592de43fb87045621424fbfe5e919
vagrant <vagrant@homestead>
2016-04-20 03:35:08 +0000

    init

---
+++ src/productsoServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace rowlin\productso;
+
+use Illuminate\Support\ServiceProvider;
+
+class productsoServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
